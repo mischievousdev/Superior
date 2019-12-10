8a710c47b7172a78ddcc6c0b496178e0981d40b9
devhubyt <57693543+devhubyt@users.noreply.github.com>
2019-12-10 07:49:15 +0530

    fixed typo

--- repo.py
+++ repo.py
@@ -1,8 +1,8 @@
 from cogs.utils import default
 
 version = "v1.0.0"
-invite = "https://discord.gg/FeD6RUs"
-owner = 589647651939549206
+invite = "https://discord.gg/qBx2uKZ"
+owner = 650890049558282272
 
 
 def is_owner(ctx):
