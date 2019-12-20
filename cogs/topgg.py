1d69e4c307e3b576285f2f04a6c98f3d62c2768a
devhubyt <57693543+devhubyt@users.noreply.github.com>
2019-12-20 08:37:18 +0530

    top.gg stuff

---
+++ cogs/topgg.py
@@ -0,0 +1,19 @@
+import dbl
+import discord
+import os
+from discord.ext import commands
+
+
+class TopGG(commands.Cog):
+    """Handles interactions with the top.gg API"""
+
+    def __init__(self, client):
+        self.client = client
+        self.token = os.getenv("API_KEY")
+        self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True)
+
+    async def on_guild_post():
+        print("Server count posted successfully")
+
+def setup(client):
+    client.add_cog(TopGG(client))
