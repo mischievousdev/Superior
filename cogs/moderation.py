74b79dea5091109aeb2a1f377d1955866dd73236
devhubyt <57693543+devhubyt@users.noreply.github.com>
2019-12-30 15:35:12 +0530

    officially v1.0.3

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -17,16 +17,12 @@ class Moderation(commands.Cog):
 
     @commands.command()
     @checks.is_mod()
-    async def kick(self, ctx, member: discord.Member = None, reason=None):
+    async def kick(self, ctx, member: discord.Member = None, *reason):
         if member == None:
             await ctx.send(f"Please Select A **Member** to kick {ctx.author.mention}")
         if member == ctx.message.author:
             await ctx.send(f"You Cannot Kick Yourself {ctx.author.mention}")
-        if reason == None:
-            reason = f"Please Specify a reason to kick {ctx.author.mention}"
-        message = f"You Have Been kicked Out from **{ctx.guild.name}**, Kicked by : {ctx.author.name}"
-        await member.send(message)
-        await ctx.guild.kick(member)
+        await member.kick()
         await ctx.channel.send(
             f"{member} has been kicked out by {ctx.author.name}", delete_after=5.0
         )
@@ -37,19 +33,13 @@ class Moderation(commands.Cog):
             await ctx.send("❗You Dont have Permission to kick")
 
     @commands.command()
-    @commands.has_permissions(ban_members=True)
     @checks.is_mod()
-    async def ban(self, ctx, member: discord.Member = None, reason=None):
+    async def ban(self, ctx, member: discord.Member = None, *reason):
         if member == None:
             await ctx.send(f"Please specify a **member** to ban {ctx.author.mention}")
         if member == ctx.message.author:
-            await ctx.send(f"You cannot ban yourself {ctx.message.author} `lol`")
-        if reason == None:
-            await ctx.send(
-                f"Please give a reason to ban the member **{ctx.author.name}**"
-            )
-        message = f"You have been banned from {ctx.guild.name} | Action done by {ctx.message.author.name}"
-        await member.send(message)
+            await ctx.send(f"You cannot ban yourself {ctx.message.author}")
+        await member.ban()
         await ctx.send(
             f"{member} has been banned by **{ctx.author.name}**", delete_after=5.0
         )
@@ -60,7 +50,6 @@ class Moderation(commands.Cog):
             await ctx.send("❕You Dont have permission to ban")
 
     @commands.command()
-    @commands.has_permissions(ban_members=True)
     @checks.is_mod()
     async def unban(self, ctx, *, member: discord.User = None):
         if member == None:
@@ -93,7 +82,6 @@ class Moderation(commands.Cog):
             await ctx.send("❕You Dont have permission to unban")
 
     @commands.command()
-    @commands.has_permissions(manage_messages=True)
     @checks.is_mod()
     async def purge(self, ctx, amount=10):
         max = 2000
@@ -110,7 +98,6 @@ class Moderation(commands.Cog):
             await ctx.send("❕You dont have Permission to purge")
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
     @checks.is_mod()
     async def mute(self, ctx, member: discord.Member = None, *, reason=None):
         if member == None:
@@ -149,7 +136,6 @@ class Moderation(commands.Cog):
             await ctx.send("❕You dont have Permission to mute")
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
     @checks.is_mod()
     async def unmute(self, ctx, member: discord.Member = None):
         if member == None:
@@ -176,12 +162,11 @@ class Moderation(commands.Cog):
             await ctx.send("❕You dont have permission to unmute")
 
     @commands.command()
-    @commands.has_permissions(ban_members=True)
     @checks.is_mod()
     async def softban(self, ctx, member: discord.Member = None, *, reason=None):
         if member == None:
             embed = discord.Embed(
-                color=0xFF2B2B,
+                color=discord.Color.blurple(),
                 title="Softban Error",
                 description="You have to specify a member to softban",
             )
@@ -189,7 +174,7 @@ class Moderation(commands.Cog):
         else:
             if member.id == ctx.message.id:
                 embed = discord.Embed(
-                    color=0xFF2B2B,
+                    color=discord.Color.blurple(),
                     title="Softban Error",
                     description="You Cannot softban yourself",
                 )
@@ -201,9 +186,9 @@ class Moderation(commands.Cog):
                     )
                     await member.unban()
                     embed = discord.Embed(
-                        color=discord.Color.blurple,
+                        color=discord.Color.blurple(),
                         title="Softban",
-                        description=f"{member.mention} has been softbanned by {ctx.message.author.mention}.\nReason - {reason}",
+                        description=reason,
                     )
                     await ctx.send(embed=embed, delete_after=10.0)
 
@@ -213,7 +198,6 @@ class Moderation(commands.Cog):
             await ctx.send("❕You dont have permission to do softban")
 
     @commands.command()
-    @commands.has_permissions(manage_server=True)
     @checks.is_mod()
     async def nuke(self, ctx):
         channel_position = ctx.channel.position
@@ -241,4 +225,3 @@ class Moderation(commands.Cog):
 
 def setup(client):
     client.add_cog(Moderation(client))
-    print("Moderation loaded")
